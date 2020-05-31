dea27c3dffc2ee6ffd6a6db445cad62135588866
pythoninja <pythoninja@users.noreply.github.com>
2020-05-31 20:32:24 +0300

    feat: add recursive flag

--- scrobblelist/cli.py
+++ scrobblelist/cli.py
@@ -9,8 +9,12 @@ from scrobblelist.utils import get_files
 @click.command()
 @click.option('-d', '--directory', type=click.Path(), required=True, help='Directory where mp3 files are stored. '
                                                                           'Non-recursive.')
-def cli(directory):
-    mp3_files = get_files(directory)
+@click.option('-r', '--recursive', type=click.BOOL, required=False, is_flag=True, help='Enables recursive search')
+def cli(directory, recursive):
+    if recursive:
+        mp3_files = get_files(directory, recursive=True)
+    else:
+        mp3_files = get_files(directory)
 
     if mp3_files:
         for file in sorted(mp3_files):
--- scrobblelist/utils.py
+++ scrobblelist/utils.py
@@ -4,8 +4,13 @@ from pathlib import Path
 from typing import List
 
 
-def get_files(files_path: str):
-    glob_mp3_pattern: str = '*.mp3'
+def get_files(files_path: str, recursive=False):
+
+    if recursive:
+        glob_mp3_pattern: str = '**/*.mp3'
+    else:
+        glob_mp3_pattern: str = '*.mp3'
+
     mp3_files: List[Path] = list(Path(files_path).glob(f'{glob_mp3_pattern}'))
 
     return mp3_files
