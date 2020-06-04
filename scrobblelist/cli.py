def944ed36c8944a67c1b88bc5f573adc9396e34
pythoninja <pythoninja@users.noreply.github.com>
2020-06-04 03:05:16 +0300

    feat: add multiple directories

--- scrobblelist/cli.py
+++ scrobblelist/cli.py
@@ -3,18 +3,16 @@
 import click
 from tinytag import TinyTag
 
-from scrobblelist.utils import get_files
+from utils import get_mp3_files
 
 
 @click.command()
-@click.option('-d', '--directory', type=click.Path(), required=True, help='Directory where mp3 files are stored. '
-                                                                          'Non-recursive.')
+@click.option('-d', '--directory', type=click.Path(), multiple=True, required=True,
+              help='Directory where mp3 files are stored. Non-recursive')
 @click.option('-r', '--recursive', type=click.BOOL, required=False, is_flag=True, help='Enables recursive search')
 def cli(directory, recursive):
-    if recursive:
-        mp3_files = get_files(directory, recursive=True)
-    else:
-        mp3_files = get_files(directory)
+
+    mp3_files = get_mp3_files(directory, recursive)
 
     if mp3_files:
         for file in sorted(mp3_files):
--- scrobblelist/utils.py
+++ scrobblelist/utils.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 
+from itertools import chain
 from pathlib import Path
-from typing import List
+from typing import List, Tuple, Iterable
+
+FIRST_DIRECTORY = 0
 
 
-def get_files(files_path: str, recursive=False):
+def get_mp3_files(directory: Tuple, recursive: bool) -> List[Path]:
+    if len(directory) == 1:
+        return scan_directory(directory[FIRST_DIRECTORY], recursive=recursive)
+    else:
+        files = []
+        for i, d in enumerate(directory):
+            files.append(scan_directory(directory[i]))
 
+        return list(chain.from_iterable(files))
+
+
+def scan_directory(files_path: str, recursive=False) -> List[Path]:
     if recursive:
         glob_mp3_pattern: str = '**/*.mp3'
     else:
         glob_mp3_pattern: str = '*.mp3'
 
-    mp3_files: List[Path] = list(Path(files_path).glob(f'{glob_mp3_pattern}'))
-
-    return mp3_files
+    return list(Path(files_path).glob(f'{glob_mp3_pattern}'))
